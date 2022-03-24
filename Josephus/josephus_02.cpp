1aff41378fa8594615071c262f3bd8191e03129b
Camila Belmont <camilabelmont.alu@gmail.com>
2022-03-24 16:18:27 -0300

    update

--- Josephus/josephus_02.cpp
+++ Josephus/josephus_02.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
-void abordagem_2(int circulo[], int inicio, int size) {
+void imprimir_vetor(int circulo[], int size, int inicio) {
+    cout << "[";
+    for (int i {0}; i < size; i++) {
+        if (circulo[i] != 0)
+            cout << " " << circulo[i];
+        if (i == inicio)
+            cout << ">";
+    }
+    cout << " ]" << endl;
+}
 
+int procurar_vivo (int circulo[], int escolhido, int size) {
+    int next = (escolhido + 1) % size;
+    while(circulo[next] == 0)
+        next = (next + 1) % size;
+    return next;
+}
+
+void abordagem_1(int circulo[], int inicio, int size) {
+    for (int i {0}; i < size; i++) {
+        imprimir_vetor(circulo, size, inicio);
+        int next = procurar_vivo(circulo, inicio, size);
+        circulo[next] = 0;
+        inicio = procurar_vivo(circulo, next, size);
+    }
 }
 
 int main() {
@@ -15,5 +38,5 @@ int main() {
     for (int i {0}; i < size; i++)
         circulo[i] = i+1;
     
-    abordagem_2(circulo, (inicio-1), size);
+    abordagem_1(circulo, (inicio-1), size);
 }
