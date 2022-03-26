73266296b693b3e714303da796c026399515c87c
Camila Belmont <camilabelmont.alu@gmail.com>
2022-03-26 15:23:00 -0300

    update

--- Estressados/Estressados_bloco2.cpp
+++ Estressados/Estressados_bloco2.cpp
@@ -6,6 +6,28 @@
 
 using namespace std;
 
+void swap(int *a, int *b) {
+    int aux = *a; 
+    *a = *b; 
+    *b = aux; 
+}
+
+void ordenar(vector<int>& fila) {
+    int size {(int) fila.size()};
+    for (int i {0}; i < (size-1); i++) {
+        int min {i};
+        for (int j {i+1}; j < size; j++) {
+            if (fila[j] < fila[min])
+                min = j;
+        }
+        swap(fila[i], fila[min]);
+    }
+}
+
+void embaralhar(vector<int>& fila) {
+    random_shuffle(fila.begin(), fila.end());
+}
+
 int sortear(const vector<int>& fila) {
     srand((unsigned) time(0));
     int size {(int) fila.size()}, randomNumber {0};
@@ -62,22 +84,26 @@ vector<int> clonar(const vector<int>& fila) {
 }
 
 void imprimir(vector<int> nome_vetor){
-    cout << "[";
+    cout << "[ ";
     for (auto vetor : nome_vetor)
-        cout << vetor << ", ";
+        cout << vetor << " ";
     cout << "]" << endl;
 }
 
 int main() {
+    vector<int> vetor {-51, 99, 1, -50, -1, -99};
     /*  FILTER  */
-    imprimir(clonar({-51, 99, 1, -50, -1, -99}));
-    imprimir(pegar_homens({-51, 99, 1, -50, -1, -99}));
-    imprimir(pegar_calmos({-51, 99, 1, -50, -1, -99}));
-    imprimir(pegar_mulheres_calmas({-51, 99, 1, -50, -1, -99}));                            
+    imprimir(clonar(vetor));
+    imprimir(pegar_homens(vetor));
+    imprimir(pegar_calmos(vetor));
+    imprimir(pegar_mulheres_calmas(vetor));                            
     /*  ACESSO  */
-    imprimir(inverter_com_copia({-51, 99, 1, -50, -1, -99}));
-    vector<int> inverter {-51, 99, 1, -50, -1, -99};
-    inverter_inplace(inverter);
-    imprimir(inverter);
-    cout << sortear({-51, 99, 1, -50, -1, -99}) << endl;
+    imprimir(inverter_com_copia(vetor));
+    inverter_inplace(vetor);
+    imprimir(vetor);
+    cout << sortear(vetor) << endl;
+    embaralhar(vetor);
+    imprimir(vetor);     
+    ordenar(vetor);
+    imprimir(vetor);                            
 }
